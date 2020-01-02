f7b7dc347901924b4dfdfc71275bfe923a2139f2
Frans <frans-de-boer@hotmail.com>
2020-01-02 17:20:03 +0100

    Removed tensorflow dependency (doesnt work in python 3.8). Added npz file for digits

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-from keras.datasets import mnist
 import random
+import pickle
+import NeuralNetwork
 import matplotlib.pyplot as plt
 import numpy as np
-import NeuralNetwork
 
 # get data from keras
 print('-=-Getting training data-=-')
-(train_x, train_y), (test_x, test_y) = mnist.load_data()
+# (train_x, train_y), (test_x, test_y) = mnist.load_data()
+with np.load('mnist.npz') as data:
+    train_x = data['training_images']
+    train_y = data['training_labels']
 
 training_data = []
 # setup training data
 for i in range(0, len(train_x)):
-    y = np.zeros((10,1))
-    y[train_y[i]-1]=1
-    training_data.append((train_x[i].reshape((28*28,1))/255,y))
+    #y = np.zeros((10,1))
+    #y[train_y[i]-1]=1
+    training_data.append((train_x[i],train_y[i]))
 
 random.shuffle(training_data)
 test_data = training_data[:5000]
 training_data = training_data[5000:]
 
 # create network
-nn = NeuralNetwork.NeuralNetwork([28*28, 28, 10])
+inputs = 28*28
+hidden = [int(inputs**0.5) for i in range(0, 5)]
+outputs = 10
+nn = NeuralNetwork.NeuralNetwork([inputs] + hidden + [outputs])
 # train network
 print('-=-Training network-=-')
-nn.train(training_data, 5, 40, 3, test_data=test_data)
+nn.train(training_data, 25, 40, 3, test_data=test_data)
 
 # test network
 print('-=-Testing network. Press q to go to next digit, terminate program to stop-=-')
-for i in range(0, len(test_x)):
-    prediction = nn.predict(np.array(test_x[i].reshape((28*28,1)))/255)
-    print('Prediction: {0}. Actual digit: {1}'.format(np.argmax(prediction)+1, test_y[i]))
-    plt.imshow(test_x[i], cmap='Greys')
-    plt.show()
+for i in range(0, len(test_data)):
+    prediction = nn.predict(np.array(test_data[i][0]))
+    num_predict = np.argmax(prediction)
+    num_actual = np.argmax(test_data[i][1])
+    print('Prediction: {0}. Actual digit: {1}'.format(num_predict, num_actual))
+    if num_predict != num_actual:
+        plt.imshow(test_data[i][0].reshape((28,28)), cmap='Greys')
+        plt.show()
